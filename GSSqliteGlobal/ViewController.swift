9b11c165a1eebcc44269ce2df5dfc657c505c6c0
igatsha <gati1993@gmail.com>
2020-02-07 00:29:18 +0530

    Initial Commit

---
+++ GSSqliteGlobal/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GSSqliteGlobal
+//
+//  Created by Gati Shah on 07/02/20.
+//  Copyright © 2020 iGatiTech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
